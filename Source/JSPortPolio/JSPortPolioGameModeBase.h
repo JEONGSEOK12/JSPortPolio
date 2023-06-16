df0c627cbc52546673efdc2e066a1dd2fbce3b8f
assor <assor@DESKTOP-R27R7BC>
2023-06-16 10:50:24 +0900

    초기 커밋.

---
+++ Source/JSPortPolio/JSPortPolioGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JSPortPolioGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JSPORTPOLIO_API AJSPortPolioGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
