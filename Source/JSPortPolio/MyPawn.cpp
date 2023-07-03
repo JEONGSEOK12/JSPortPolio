8db26e43e6cdf3629fe5763a005a671d2f83250f
assor <assor@DESKTOP-R27R7BC>
2023-07-04 07:54:58 +0900

    123123

--- Source/JSPortPolio/MyPawn.cpp
+++ Source/JSPortPolio/MyPawn.cpp
@@ -30,5 +30,99 @@ void AMyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	static bool bBindingsAdded = false;
+	if (!bBindingsAdded)
+	{
+		bBindingsAdded = true;
+
+		// 여기의 내용은 뭐냐?
+		// DefaultPawn_MoveForward 추가되는것 뿐
+		// 축매핑만 하고 있스니다.
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("PlayerMoveForward", EKeys::W, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("PlayerMoveForward", EKeys::S, -1.f));
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("PlayerMoveRight", EKeys::A, -1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("PlayerMoveRight", EKeys::D, 1.f));
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("PlayerTurn", EKeys::MouseX, 1.f));
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("PlayerLookUp", EKeys::MouseY, -1.f));
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("PlayerLookUp", EKeys::MouseY, -1.f));
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("PlayerJumpAxis", EKeys::E, -1.f));
+
+
+		UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping(TEXT("PlayerJumpAction"), EKeys::SpaceBar));
+	}
+
+	// 키와 함수를 연결합니다.
+	// 이 키가 눌리면 이 함수를 실행시켜줘인데.
+	// 축일때는 일단 계속 실행시켜줘.
+	PlayerInputComponent->BindAxis("PlayerMoveForward", this, &AMyPawn::MoveForward);
+	PlayerInputComponent->BindAxis("PlayerMoveRight", this, &AMyPawn::MoveRight);
+	PlayerInputComponent->BindAxis("PlayerTurn", this, &AMyPawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("PlayerTurnRate", this, &AMyPawn::TurnAtRate);
+	PlayerInputComponent->BindAxis("PlayerLookUp", this, &AMyPawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("PlayerLookUpRate", this, &AMyPawn::LookUpAtRate);
+	PlayerInputComponent->BindAxis("PlayerJumpAxis", this, &AMyPawn::JumpAxis);
+
+	PlayerInputComponent->BindAction("PlayerJumpAction", EInputEvent::IE_Pressed, this, &AMyPawn::JumpAction);
+
+
+
+	
+
 }
+
+
+void AMyPawn::MoveRight(float Val)
+{
+	if (Val != 0.f)
+	{
+		if (Controller)
+		{
+			FRotator const ControlSpaceRot = Controller->GetControlRotation();
+			// transform to world space and add it
+			// 현재 내 회전을 가져와서 y축에 해당하는 축벡터를 얻어오는 겁니다.
+			AddMovementInput(FRotationMatrix(ControlSpaceRot).GetScaledAxis(EAxis::Y), Val);
+		}
+	}
+}
+
+void AMyPawn::MoveForward(float Val)
+{
+	if (Val != 0.f)
+	{
+		if (Controller)
+		{
+			// 컨트롤러는 기본적으로
+			// charcter 하나씩 붙어 있습니다.
+			FRotator const ControlSpaceRot = Controller->GetControlRotation();
+
+			// 이건 방향일 뿐입니다.
+			// transform to world space and add it
+			AddMovementInput(FRotationMatrix(ControlSpaceRot).GetScaledAxis(EAxis::X), Val);
+			// 탑뷰게임이면 다르게 나오게 되는데.
+			// 지금은 TPS를 하고 있기 때문에 컨트롤러의 회전이나 액터의 회전이나 같아요.
+			// AddMovementInput(GetActorForwardVector(), Val);
+		}
+	}
+}
+
+
+void AMyPawn::TurnAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds() * CustomTimeDilation);
+}
+
+void AMyPawn::LookUpAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds() * CustomTimeDilation);
+}
+
+
+
 
--- Source/JSPortPolio/MyPawn.h
+++ Source/JSPortPolio/MyPawn.h
@@ -19,11 +19,26 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	void JumpAction();
+	void JumpAxis(float Rate);
+
+	void MoveRight(float Val);
+	void MoveForward(float Val);
+	void TurnAtRate(float Rate);
+	void LookUpAtRate(float Rate);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pawn")
+		float BaseTurnRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pawn")
+		float BaseLookUpRate;
+
+	bool AxisJump = false;
 };
